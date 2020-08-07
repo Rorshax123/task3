52912e0ef8f56a80053beccc9dd76908f973154b
Rorshax123 <karimjonovhasanjon@gmail.com>
2020-08-07 17:24:59 +0500

    Check the strings on test

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,14 @@
 public class MainClass {
     private int classNumber = 20;
 
+    private String classString = "Hello, world";
+
+
     public int getClassNumber(){
         return classNumber;
+    }
+
+    public String getClassString(){
+        return classString;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,4 +6,10 @@ public class MainClassTest extends MainClass{
     public void testGetClassNumber(){
         Assert.assertTrue("ClassNumber no more than 45", getClassNumber() > 45);
     }
+
+    @Test
+    public void testGetClassString(){
+
+        Assert.assertTrue("ClassString has not hello or Hello", getClassString().contains("hello") || getClassString().contains("Hello") );
+    }
 }
